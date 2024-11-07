7b83f42c4feb7bc1f8cbb50492946fbada91d06d
villaceranlody <146922763+villaceranlody@users.noreply.github.com>
2024-11-07 14:39:32 +0800

    Add files via upload

---
+++ lody php/index.php
@@ -0,0 +1,12 @@
+	<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>lab2</title>
+</head>
+<body>
+	
+	<p>My first ubuntu server PHP Deployment</p>
+</body>
+</html>
